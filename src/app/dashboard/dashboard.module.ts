f435043d3dd0b33cf87e03923054e299cc1c572f
Tyler Comstock <comstocktylerj@yahoo.com>
2021-04-13 12:34:06 -0400

    set up routing

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,8 +4,11 @@ import { StatFiltersComponent } from './stat-filters/stat-filters.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +18,8 @@ import { VideoDashboardComponent } from './video-dashboard/video-dashboard.compo
     VideoDashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DashboardModule { }
